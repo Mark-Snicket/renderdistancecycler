204e23cf2e380c44216594cd8e00a42b2c4a4ea0
Mark <3mark.7395g@gmail.com>
2025-09-29 01:33:06 +0200

    updated the code to the actual source code, didn't get GitHub to work on IntelliJ before. Now I finally was, so here's the real code :)

--- src/main/java/net/mark/renderdistancecycler/mixin/KeyboardMixin.java
+++ src/main/java/net/mark/renderdistancecycler/mixin/KeyboardMixin.java
@@ -4,6 +4,8 @@ import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.option.SimpleOption;
+import net.minecraft.text.MutableText;
+import net.minecraft.text.Text;
 import net.minecraft.util.math.MathHelper;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
@@ -22,15 +24,25 @@ public abstract class KeyboardMixin {
     @Shadow
     protected abstract void debugLog(String key);
 
+
+    private void sendMessage(MutableText translatable) {}
+
     @Inject(method = "processF3", at = @At("RETURN"), cancellable = true)
     public void tryCycleRenderDistance(int key, CallbackInfoReturnable<Boolean> cir) {
         if (!cir.getReturnValue() && key == 70) {
             SimpleOption<Integer> renderDistance = client.options.getViewDistance();
             SimpleOption.ValidatingIntSliderCallbacks callbacks = (SimpleOption.ValidatingIntSliderCallbacks) renderDistance.getCallbacks();
+
             renderDistance.setValue(MathHelper.clamp(renderDistance.getValue() + (Screen.hasShiftDown() ? -1 : 1), callbacks.minInclusive(), callbacks.maxInclusive()));
             this.debugLog("debug.cycle_renderdistance.message." + renderDistance.getValue());
             cir.setReturnValue(true);
         }
     }
+                /* not required, so comment out for now */
+    //@Inject(method = "processF3", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;)V", ordinal = 4))
+    public void addCycleRenderHelpMessage(int key, CallbackInfoReturnable<Boolean> cir) {
+        this.sendMessage(Text.translatable("debug.cycle_renderdistance.help"));
+    }
+
 }
 
