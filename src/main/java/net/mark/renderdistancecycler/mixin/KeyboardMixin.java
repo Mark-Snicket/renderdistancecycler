089584e4484e2c6d546471ab474e30c8856220e3
Mark <3mark.7395g@gmail.com>
2025-09-29 02:09:53 +0200

    - update to 1.21.9 - discord invite link fixed

--- src/main/java/net/mark/renderdistancecycler/mixin/KeyboardMixin.java
+++ src/main/java/net/mark/renderdistancecycler/mixin/KeyboardMixin.java
@@ -3,6 +3,7 @@ package net.mark.renderdistancecycler.mixin;
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.screen.Screen;
+import net.minecraft.client.input.KeyInput;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.text.MutableText;
 import net.minecraft.text.Text;
@@ -28,21 +29,20 @@ public abstract class KeyboardMixin {
     private void sendMessage(MutableText translatable) {}
 
     @Inject(method = "processF3", at = @At("RETURN"), cancellable = true)
-    public void tryCycleRenderDistance(int key, CallbackInfoReturnable<Boolean> cir) {
-        if (!cir.getReturnValue() && key == 70) {
+    public void tryCycleRenderDistance(KeyInput input, CallbackInfoReturnable<Boolean> cir) {
+        if (!cir.getReturnValue() && input.key() == 70) {
             SimpleOption<Integer> renderDistance = client.options.getViewDistance();
             SimpleOption.ValidatingIntSliderCallbacks callbacks = (SimpleOption.ValidatingIntSliderCallbacks) renderDistance.getCallbacks();
 
-            renderDistance.setValue(MathHelper.clamp(renderDistance.getValue() + (Screen.hasShiftDown() ? -1 : 1), callbacks.minInclusive(), callbacks.maxInclusive()));
+            renderDistance.setValue(MathHelper.clamp(renderDistance.getValue() + (input.hasShift() ? -1 : 1), callbacks.minInclusive(), callbacks.maxInclusive()));
             this.debugLog("debug.cycle_renderdistance.message." + renderDistance.getValue());
             cir.setReturnValue(true);
         }
     }
-                /* not required, so comment out for now */
+    /* not required, so comment out for now */
     //@Inject(method = "processF3", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;)V", ordinal = 4))
     public void addCycleRenderHelpMessage(int key, CallbackInfoReturnable<Boolean> cir) {
         this.sendMessage(Text.translatable("debug.cycle_renderdistance.help"));
     }
 
 }
-
