6f33e9db1c899b7720534d02b4dd52b2ae1c7cbe
Mark <3mark.7395g@gmail.com>
2025-10-07 21:12:56 +0200

    _

--- src/main/java/net/mark/renderdistancecycler/mixin/KeyboardMixin.java
+++ src/main/java/net/mark/renderdistancecycler/mixin/KeyboardMixin.java
@@ -2,7 +2,6 @@ package net.mark.renderdistancecycler.mixin;
 
 import net.minecraft.client.Keyboard;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.input.KeyInput;
 import net.minecraft.client.option.SimpleOption;
 import net.minecraft.text.MutableText;
