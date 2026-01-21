208d4f86815e253e1b640f28306b96fa8d613b4c
Mark <3mark.7395g@gmail.com>
2026-01-21 22:03:01 +0100

    -implemented addCycleRenderHelpMessage() into tryCycleRenderDistance() in a functional way - added issues link to fabric.mod.json - excluded 1.21.11 as a compatible version - made version number in fabric.mod.json use the version specified in gradle.properties & updated it to 1.0.2

--- src/main/java/net/mark/renderdistancecycler/mixin/KeyboardMixin.java
+++ src/main/java/net/mark/renderdistancecycler/mixin/KeyboardMixin.java
@@ -23,29 +23,23 @@ public abstract class KeyboardMixin {
     private Minecraft minecraft;
 
     @Shadow
+    protected abstract void showDebugChat(Component translatable);
+
+    @Shadow
     protected abstract void debugFeedbackComponent(Component component);
 
-
-    private void debugLog(String key, Object[] value) {
-        this.debugFeedbackComponent(MutableComponent.create(new TranslatableContents(key, (String)null, value)));
-    }
-
     @Inject(method = "handleDebugKeys", at = @At("RETURN"), cancellable = true)
-    public void tryCycleRenderDistance(KeyEvent input, CallbackInfoReturnable<Boolean> cir) {
-        if (!cir.getReturnValue() && input.key() == 70) {
+    public void sendHelpMessageOrCycleRenderDistance(KeyEvent input, CallbackInfoReturnable<Boolean> cir) {
+        if (input.key() == 81) {
+            this.showDebugChat(Component.translatable("debug.cycle_renderdistance.help"));
+        } else if (!cir.getReturnValue() && input.key() == 70) {
             OptionInstance<Integer> renderDistance = minecraft.options.renderDistance();
             OptionInstance.IntRange callbacks = (OptionInstance.IntRange) renderDistance.values();
 
             renderDistance.set(Mth.clamp(renderDistance.get() + (input.hasShiftDown() ? -1 : 1), callbacks.minInclusive(), callbacks.maxInclusive()));
-            this.debugLog("debug.cycle_renderdistance.message", new Integer[]{renderDistance.get()});
+            this.debugFeedbackComponent(MutableComponent.create(new TranslatableContents("debug.cycle_renderdistance.message", null, new Integer[]{renderDistance.get()})));
+
             cir.setReturnValue(true);
         }
     }
-    /* not required, so comment out for now *//*
-    //@Inject(method = "handleDebugKeys", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/hud/ChatHud;addMessage(Lnet/minecraft/text/Text;)V", ordinal = 4))
-    public void addCycleRenderHelpMessage(KeyEvent keyEvent, CallbackInfoReturnable<Boolean> cir) {
-        this.sendMessage(Component.translatable("debug.cycle_renderdistance.help"));
-    }
-
-    private void sendMessage(MutableComponent translatable) {}*/
 }
