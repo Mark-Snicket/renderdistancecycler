49a306b18f7858e9d208ea83e0ad180f89463179
Mark <3mark.7395g@gmail.com>
2026-08-15 20:29:04 +0200

    made commands client sided

--- src/main/java/net/mark/renderdistancecycler/RenderDistanceCyclerClient.java
+++ src/main/java/net/mark/renderdistancecycler/RenderDistanceCyclerClient.java
@@ -3,12 +3,12 @@ package net.mark.renderdistancecycler;
 import com.mojang.brigadier.arguments.IntegerArgumentType;
 import com.mojang.brigadier.context.CommandContext;
 import net.fabricmc.api.ClientModInitializer;
-import net.fabricmc.fabric.api.command.v2.CommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommandRegistrationCallback;
+import net.fabricmc.fabric.api.client.command.v2.ClientCommands;
+import net.fabricmc.fabric.api.client.command.v2.FabricClientCommandSource;
 import net.minecraft.ChatFormatting;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.OptionInstance;
-import net.minecraft.commands.CommandSourceStack;
-import net.minecraft.commands.Commands;
 import net.minecraft.network.chat.CommonComponents;
 import net.minecraft.network.chat.Component;
 import net.minecraft.network.chat.MutableComponent;
@@ -22,42 +22,42 @@ public class RenderDistanceCyclerClient implements ClientModInitializer {
     @Override
     public void onInitializeClient() {
         // renderDistance command
-        CommandRegistrationCallback.EVENT.register(
-                ((dispatcher, _, _) -> dispatcher.register(
-                        Commands.literal("renderDistance")
+        ClientCommandRegistrationCallback.EVENT.register(
+                ((dispatcher, _) -> dispatcher.register(
+                        ClientCommands.literal("renderDistance")
                                 .executes(RenderDistanceCyclerClient::getRenderDistance)
-                                .then(Commands.argument("target_render_distance", IntegerArgumentType.integer( 2))
+                                .then(ClientCommands.argument("target_render_distance", IntegerArgumentType.integer(2))
                                         .executes(RenderDistanceCyclerClient::setRenderDistance)))));
         // rd command
-        CommandRegistrationCallback.EVENT.register(
-                ((dispatcher, _, _) -> dispatcher.register(
-                        Commands.literal("rd")
+        ClientCommandRegistrationCallback.EVENT.register(
+                ((dispatcher, _) -> dispatcher.register(
+                        ClientCommands.literal("rd")
                                 .executes(RenderDistanceCyclerClient::getRenderDistance)
-                                .then(Commands.argument("target_render_distance", IntegerArgumentType.integer(2))
+                                .then(ClientCommands.argument("target_render_distance", IntegerArgumentType.integer(2))
                                         .executes(RenderDistanceCyclerClient::setRenderDistance)))));
     }
 
-    private static int setRenderDistance(CommandContext<CommandSourceStack> context) {
+    private static int setRenderDistance(CommandContext<FabricClientCommandSource> context) {
         OptionInstance<Integer> renderDistance = Minecraft.getInstance().options.renderDistance();
         OptionInstance.IntRange range = (OptionInstance.IntRange) renderDistance.values();
 
         renderDistance.set(Mth.clamp(IntegerArgumentType.getInteger(context, "target_render_distance"), range.minInclusive(), range.maxInclusive()));
 
         Objects.requireNonNull(
-            context.getSource().getPlayer()).sendSystemMessage(
+                context.getSource().getPlayer()).sendSystemMessage(
                 getDebugMessage(MutableComponent.create(new TranslatableContents(
-                        "debug.cycle_renderdistance.message", null, new Integer[]{renderDistance.get()}))), false);
+                        "debug.cycle_renderdistance.message", null, new Integer[]{renderDistance.get()}))));
         Minecraft.getInstance().options.save();
         return 0;
     }
 
-    private static int getRenderDistance(CommandContext<CommandSourceStack> context) {
+    private static int getRenderDistance(CommandContext<FabricClientCommandSource> context) {
         Objects.requireNonNull(
-            context.getSource().getPlayer()).sendSystemMessage(
+                context.getSource().getPlayer()).sendSystemMessage(
                 getDebugMessage(MutableComponent.create(new TranslatableContents(
                         "debug.cycle_renderdistance.message", null, new Integer[]{
                         Minecraft.getInstance().options.renderDistance().get()
-                }))), false);
+                }))));
         return 0;
     }
 
